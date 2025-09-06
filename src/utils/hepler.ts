82e1ea68e7e739fbb53714aa10e65fe581e7749d
Besher-Hamze <besherhamze97@gmail.com>
2025-09-06 17:12:53 +0300

    Update getImageUrl function to use NEXT_PUBLIC_API_URL_STATIC for image URL construction, ensuring correct URL handling for static assets.

--- src/utils/hepler.ts
+++ src/utils/hepler.ts
@@ -1,6 +1,6 @@
 function getImageUrl(imageUrl?: string) {
     console.log(imageUrl);
-    return `${process.env.NEXT_PUBLIC_API_URL}${imageUrl}`;
+    return `${process.env.NEXT_PUBLIC_API_URL_STATIC}${imageUrl}`;
     // if (!imageUrl) return '/logo.jpg';
     // return `http://localhost:3000${imageUrl}`;
 }
